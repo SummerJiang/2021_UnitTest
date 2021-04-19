0bd50381262ec4c67c8ced08f6355524e9f57191
SummerJiang <peishan1216@gmail.com>
2021-04-19 11:15:38 +0800

    調整成介面

--- 20210119_test/SLOID_Cart .cs
+++ 20210119_test/SLOID_Cart .cs
@@ -48,34 +48,24 @@ namespace SLOID_CartTest
             }
         }
 
-        public class shippingFee
+        public static double shippingFee(string shipper, double length, double width, double height, double weight)
         {
-            public static double BlackCat(string shipper, double length, double width, double height, double weight)
+            IshippingFee fee = null;
+            switch (shipper)
             {
-                if (weight > 20)
-                    return 500;
-                else
-                    return 100 + weight * 10;
+                case "black cat":
+                    fee = new BlackCat();
+                    break;
+                case "hsinchu":
+                    fee = new Hsinchu();
+                    break;
+                case "post office":
+                    fee = new Postoffice();
+                    break;
+                default:
+                    throw new ArgumentException("shipper not exist");
             }
-
-            public static double Hsinchu(string shipper, double length, double width, double height, double weight)
-            {
-                double size = length * width * height;
-                if (length > 100 || width > 100 || height > 100)
-                    return size * 0.00002 * 1100 + 500;
-                else
-                    return size * 0.00002 * 1200;
-            }
-
-            public static double Postoffice(string shipper, double length, double width, double height, double weight)
-            {
-                double feeByWeight = 80 + weight * 10;
-                double size = length * width * height;
-                double feeBySize = size * 0.00002 * 1100;
-                return feeByWeight < feeBySize ? feeByWeight : feeBySize;
-            }
-
-          
+            return fee;
         }
     }
 }
