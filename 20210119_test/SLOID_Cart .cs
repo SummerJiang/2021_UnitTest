ff616d2b7b6549f0fac82b7548814078ddf07ce8
SummerJiang <peishan1216@gmail.com>
2021-04-19 10:42:59 +0800

    SOLID

---
+++ 20210119_test/SLOID_Cart .cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SLOID_CartTest
+{
+    public class Cart
+    {
+        public interface IshippingFee
+        {
+            double shippingFee(string shipper, double length, double width, double height, double weight);
+        }
+
+        public class BlackCat : IshippingFee
+        {
+            public double shippingFee(string shipper, double length, double width, double height, double weight)
+            {
+                if (weight > 20)
+                    return 500;
+                else
+                    return 100 + weight * 10;
+            }
+        }
+
+        public class Hsinchu : IshippingFee
+        {
+            public double shippingFee(string shipper, double length, double width, double height, double weight)
+            {
+                double size = length * width * height;
+                if (length > 100 || width > 100 || height > 100)
+                    return size * 0.00002 * 1100 + 500;
+                else
+                    return size * 0.00002 * 1200;
+            }
+        }
+
+        public class Postoffice : IshippingFee
+        {
+            public double shippingFee(string shipper, double length, double width, double height, double weight)
+            {
+                double feeByWeight = 80 + weight * 10;
+                double size = length * width * height;
+                double feeBySize = size * 0.00002 * 1100;
+                return feeByWeight < feeBySize ? feeByWeight : feeBySize;
+            }
+        }
+
+        public class shippingFee
+        {
+            public static double BlackCat(string shipper, double length, double width, double height, double weight)
+            {
+                if (weight > 20)
+                    return 500;
+                else
+                    return 100 + weight * 10;
+            }
+
+            public static double Hsinchu(string shipper, double length, double width, double height, double weight)
+            {
+                double size = length * width * height;
+                if (length > 100 || width > 100 || height > 100)
+                    return size * 0.00002 * 1100 + 500;
+                else
+                    return size * 0.00002 * 1200;
+            }
+
+            public static double Postoffice(string shipper, double length, double width, double height, double weight)
+            {
+                double feeByWeight = 80 + weight * 10;
+                double size = length * width * height;
+                double feeBySize = size * 0.00002 * 1100;
+                return feeByWeight < feeBySize ? feeByWeight : feeBySize;
+            }
+
+          
+        }
+    }
+}
---
+++ 20210119_test/SLOID_Cart _2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SLOID_CartTest_2
+{
+    //public abstract class Cart
+    //{
+    //    public abstract double shippingFee(string shipper, double length, double width, double height, double weight)
+    //    {
+    //        double fee = 0;
+    //        switch (shipper)
+    //        { 
+    //            case "black cat":
+    //                BlackCat bc = new BlackCat();
+    //                fee = bc.shippingFee();
+    //                break;
+    //            case "hsinchu":
+    //                Hsinchu hc = new Hsinchu();
+    //                fee = hc.shippingFee();
+    //                break;
+    //            case "post office":
+    //                Hsinchu po = new Hsinchu();
+    //                fee = po.shippingFee();
+    //                break;
+    //            default:
+    //                throw new ArgumentException("shipper not exist");
+    //        }
+    //        return fee;
+
+    //    }
+     
+    //}
+    //public class BlackCat : Cart
+    //{
+    //    public override double shippingFee(string shipper, double length, double width, double height, double weight)
+    //    {
+    //        if (weight > 20)
+    //            return 00;
+    //        else
+    //            return 100 + weight * 10;
+    //    }
+    //}
+
+    //class Hsinchu : Cart
+    //{
+    //    public override double shippingFee(string shipper, double length, double width, double height, double weight)
+    //    {
+    //        double size = length * width * height;
+    //        if (length > 100 || width > 100 || height > 100)
+    //            return size * 0.00002 * 1100 + 500;
+    //        else
+    //            return size * 0.00002 * 1200;
+    //    }
+    //}
+
+    //class Postoffice : Cart
+    //{
+    //    public override double shippingFee(string shipper, double length, double width, double height, double weight)
+    //    {
+    //        double feeByWeight = 80 + weight * 10;
+    //        double size = length * width * height;
+    //        double feeBySize = size * 0.00002 * 1100;
+    //        return feeByWeight < feeBySize ? feeByWeight : feeBySize;
+    //    }
+
+
+
+    //}
+
+}
